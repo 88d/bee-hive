9a0aa24b065a8eaaa8b8e80cf902bba2b69fd383
Opiskull <Christian.Onuk@gmx.net>
2016-06-17 23:16:18 +0200

    feat(questions): add auto creation of table and database

--- questions/repository.go
+++ questions/repository.go
@@ -2,6 +2,8 @@ package questions
 
 import r "github.com/dancannon/gorethink"
 
+import "log"
+
 type Repository struct {
 	session  *r.Session
 	database string
@@ -12,6 +14,65 @@ func (re Repository) Table() r.Term {
 	return r.DB(re.database).Table(re.table)
 }
 
+func initDB(session *r.Session, dbName string) error {
+	log.Println("initDB", dbName)
+	dbListRes, err := r.DBList().Run(session)
+	if err != nil {
+		return err
+	}
+	var dbs []string
+	if err := dbListRes.All(&dbs); err != nil {
+		return err
+	}
+	if !isStringInArray(dbName, dbs) {
+		log.Println("Create DB", dbName)
+		_, errCreateDB := r.DBCreate(dbName).RunWrite(session)
+		if errCreateDB != nil {
+			return errCreateDB
+		}
+	}
+	return nil
+}
+
+func initTable(session *r.Session, dbName string, table string) error {
+	log.Println("initTable", table, "in DB", dbName)
+	tableListRes, err := r.DB(dbName).TableList().Run(session)
+	if err != nil {
+		return err
+	}
+	var tables []string
+	if err := tableListRes.All(&tables); err != nil {
+		return err
+	}
+	if !isStringInArray(table, tables) {
+		log.Println("Create table", table, "in DB", dbName)
+		_, errCreateDB := r.DB(dbName).TableCreate(table).RunWrite(session)
+		if errCreateDB != nil {
+			panic(errCreateDB)
+		}
+	}
+	return nil
+}
+
+func initRepository(repo Repository) error {
+	if err := initDB(repo.session, repo.database); err != nil {
+		return err
+	}
+	if err := initTable(repo.session, repo.database, repo.table); err != nil {
+		return err
+	}
+	return nil
+}
+
+func isStringInArray(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
 func NewRepository(server string, database string) Repository {
 	var session *r.Session
 	session, err := r.Connect(r.ConnectOpts{
@@ -20,10 +81,15 @@ func NewRepository(server string, database string) Repository {
 	if err != nil {
 		panic(err)
 	}
-	return Repository{session, database, "questions"}
+	var repo = Repository{session, database, "questions"}
+	if err := initRepository(repo); err != nil {
+		panic(err)
+	}
+	return repo
 }
 
 func (re Repository) GetAll() ([]Question, error) {
+	re.session.Query(r.Query{})
 	res, err := re.Table().Run(re.session)
 	if err != nil {
 		return nil, err
