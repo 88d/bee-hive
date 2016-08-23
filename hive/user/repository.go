fc52987abd43ea0dbd81e11c209825e7ba882cd6
Opiskull <Christian.Onuk@gmx.net>
2016-08-23 19:25:08 +0200

    feat(users): add basic users functionality

---
+++ hive/user/repository.go
@@ -0,0 +1,64 @@
+package user
+
+import "github.com/black-banana/bee-hive/rethink"
+
+var TableName = "users"
+
+type Repository struct {
+	rethink.Repository
+}
+
+var repository *Repository
+
+func NewRepository() *Repository {
+	return &Repository{rethink.NewRepository(TableName)}
+}
+
+func (re *Repository) GetAll() ([]*User, error) {
+	res, err := re.Table().Run(re.Session)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Close()
+	var users = make([]*User, 0)
+	if err := res.All(&users); err != nil {
+		return nil, err
+	}
+	return users, err
+}
+
+func (re *Repository) Create(q *User) error {
+	res, err := re.Table().Insert(q).RunWrite(re.Session)
+	if err != nil {
+		return err
+	}
+	q.ID = res.GeneratedKeys[0]
+	return nil
+}
+
+func (re *Repository) Update(q *User) error {
+	if _, err := re.Table().Get(q.ID).Update(q).RunWrite(re.Session); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (re *Repository) GetByID(id string) (*User, error) {
+	res, err := re.Table().Get(id).Run(re.Session)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Close()
+	var q *User
+	if err := res.One(&q); err != nil {
+		return nil, err
+	}
+	return q, nil
+}
+
+func (re *Repository) RemoveByID(id string) error {
+	if _, err := re.Table().Get(id).Delete().RunWrite(re.Session); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ hive/user/user.go
@@ -0,0 +1,6 @@
+package user
+
+type User struct {
+	ID   string `gorethink:"id,omitempty" json:"id,omitempty"`
+	Name string `gorethink:"name,omitempty" json:"name,omitempty"`
+}
