577ae55f11e48cea2a1fa9785aa98e44131eed53
Opiskull <Christian.Onuk@gmx.net>
2016-08-31 21:52:54 +0200

    feat(hub): add auth to hub

--- hive/auth/claims.go
+++ hive/auth/claims.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 
@@ -20,32 +19,14 @@ func (j *JwtCustomClaims) HasAccess(scope string) bool {
 	return isStringInArray(j.Scopes, scope)
 }
 
-const (
-	bearer = "Bearer"
-	header = "Authorization"
-)
-
-// jwtFromHeader returns a `jwtExtractor` that extracts token from request header.
-func JWTFromHeader(r *http.Request) (string, error) {
-	auth := r.Header.Get(header)
-	l := len(bearer)
-	if len(auth) > l+1 && auth[:l] == bearer {
-		return auth[l+1:], nil
-	}
-	return "", errors.New("empty or invalid jwt in request header")
-}
-
-func ClaimsFromRequest(r *http.Request) (*JwtCustomClaims, error) {
-	tokenString, err := JWTFromHeader(r)
-	if err != nil {
-		return nil, err
-	}
-	token, err := jwt.ParseWithClaims(tokenString, config.Claims, func(t *jwt.Token) (interface{}, error) {
+func ClaimsFromRequestQuery(r *http.Request) (*JwtCustomClaims, error) {
+	tokenString := r.URL.Query().Get("token")
+	token, err := jwt.ParseWithClaims(tokenString, &JwtCustomClaims{}, func(t *jwt.Token) (interface{}, error) {
 		// Check the signing method
 		if t.Method.Alg() != "HS256" {
 			return nil, fmt.Errorf("unexpected jwt signing method=%v", t.Header["alg"])
 		}
-		return config.SigningKey, nil
+		return []byte(config.SigningKey), nil
 	})
 	if err == nil && token.Valid {
 		return token.Claims.(*JwtCustomClaims), nil
